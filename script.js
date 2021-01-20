9a7521c8e0dd1a83225bc92e96d3350cef723b48
s3eeds <s.s.masaod@gmail.com>
2021-01-20 09:22:02 +0000

    final

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function compute(){
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     result = document.getElementById("result");
-     result.innerHTML =(p*r*y/100);
+     result.innerHTML ="If you deposit"+" "+p+","+"<br />"+"at an interest rate of "+r+"%."+"<br />"+"You will receive an amount of "+(p*r*y/100)+","+"<br />"+"in the year"+(+2020+ +y);
 }
 function show_value(x)
 {
