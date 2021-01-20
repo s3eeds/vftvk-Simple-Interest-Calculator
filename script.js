ad2e53a47eab1d61311e81600df48de85b8e3ea2
s3eeds <s.s.masaod@gmail.com>
2021-01-20 08:39:35 +0000

    final

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-function compute()
-{
+function compute(){
+
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    result = document.getElementById("result");
+     result.innerHTML =(p*r*y/100);
 }
-        
+function show_value(x)
+{
+ document.getElementById("slider_value").innerHTML=x;
+}
