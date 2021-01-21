87e2eb55bdc4481183db9b0d5c702184e5abb15f
s3eeds <s.s.masaod@gmail.com>
2021-01-21 17:13:17 +0000

    finish

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 function compute(){
-
-    p = document.getElementById("principal").value;
-    r = document.getElementById("rate").value;
-    y = document.getElementById("years").value;
-    result = document.getElementById("result");
-     result.innerHTML ="If you deposit"+" "+p+","+"<br />"+"at an interest rate of "+r+"%."+"<br />"+"You will receive an amount of "+(p*r*y/100)+","+"<br />"+"in the year"+(+2020+ +y);
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rangeRate").value;
+    var years = document.getElementById("years").value;
+    var fut = convertYears(years);
+    var interest = principal * years * rate / 100;
+    if(validateForm()){
+        document.getElementById("result").innerHTML=
+        "If you deposit <mark>"+ principal +"</mark>,<br/>"+
+        " at an interest rate of <mark>"+ rate +"</mark>.<br/>"+
+        " You will receive an amount of <mark>"+ interest +"</mark>,<br/>"+
+        " in the year <mark>" + fut+"</mark>";
+    }
 }
-function show_value(x)
-{
- document.getElementById("slider_value").innerHTML=x;
+
+function readRate(val){
+    document.getElementById("rate").innerHTML=val;
+}
+
+function convertYears(years){
+    var date = new Date(); 
+    var year = date.getFullYear();
+    var future = parseInt(year) + parseInt(years);
+    return future;
+}
+    
+
+function validateForm() {
+  var principal = document.getElementById("principal").value;
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus(); 
+    return false;
+  }
+  return true
 }
